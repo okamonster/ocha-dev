d087e459bd442e727312006a39de5ec71bb7eb1f
okamonster <okamonster0615@gmail.com>
2023-07-27 02:41:17 +0900

    fix:prettier setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import "@/styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS>
       <Component {...pageProps} />
     </ChakraProvider>
-  );
+  )
 }
