bcee5baba2870a866a1cd76cbcf576df506dce87
okamonster <okamonster0615@gmail.com>
2023-09-01 09:55:52 +0900

    feat:skillコンテンツの追加

--- src/components/TopPage/Skills.tsx
+++ src/components/TopPage/Skills.tsx
@@ -1,16 +1,120 @@
 import { Box, SimpleGrid, Image, Badge, Text } from '@chakra-ui/react'
 import { SectionTitle } from '@/components/TopPage/SectionTitle'
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
 import { LinkButton } from '../Button/LinkButton'
 import { fadeIn } from '@/constants/animation'
 
-const cardContainer = css({
-  borderRadius: 8,
-  background: '#FFFFFF',
-  padding: 4,
-  display: 'flex',
-})
+const skills = [
+  {
+    imageUrl: '/images/html.png',
+    name: 'HTML',
+    categorie: ['markup'],
+    genle: ['web', 'front-end'],
+  },
+  {
+    imageUrl: '/images/css.png',
+    name: 'CSS',
+    categorie: ['markup'],
+    genle: ['web', 'front-end'],
+  },
+  {
+    imageUrl: '/images/javascript.png',
+    name: 'JavaScript',
+    categorie: ['language'],
+    genle: ['web', 'front-end'],
+  },
+  {
+    imageUrl: '/images/typescript.png',
+    name: 'TypeScript',
+    categorie: ['language'],
+    genle: ['web', 'front-end', 'back-end'],
+  },
+  {
+    imageUrl: '/images/php.png',
+    name: 'PHP',
+    categorie: ['language'],
+    genle: ['web', 'back-end'],
+  },
+  {
+    imageUrl: '/images/java.png',
+    name: 'Java',
+    categorie: ['language'],
+    genle: [],
+  },
+  {
+    imageUrl: '/images/python.png',
+    name: 'Python',
+    categorie: ['language'],
+    genle: ['web', 'back-end', 'ML'],
+  },
+  {
+    imageUrl: '/images/matlab.png',
+    name: 'MATLAB',
+    categorie: ['language'],
+    genle: ['Math', '嫌い'],
+  },
+  {
+    imageUrl: '/images/verilog.png',
+    name: 'Verilog',
+    categorie: ['language'],
+    genle: ['HDL', 'めっちゃ嫌い'],
+  },
+  {
+    imageUrl: '/images/express.png',
+    name: 'Express',
+    categorie: ['framework'],
+    genle: ['web', 'back-end'],
+  },
+  {
+    imageUrl: '/images/react.png',
+    name: 'React.js',
+    categorie: ['framework'],
+    genle: ['web', 'front-end'],
+  },
+  {
+    imageUrl: '/images/next.png',
+    name: 'Next.js',
+    categorie: ['framework'],
+    genle: ['web', 'front-end'],
+  },
+  {
+    imageUrl: '/images/three.png',
+    name: 'Three.js',
+    categorie: ['library'],
+    genle: ['webgl', 'XR'],
+  },
+  {
+    imageUrl: '/images/mysql.png',
+    name: 'MySQL',
+    categorie: ['DataBase'],
+    genle: ['RDB'],
+  },
+  {
+    imageUrl: '/images/apache.png',
+    name: 'Apache',
+    categorie: ['Runtime'],
+    genle: ['WebServer'],
+  },
+  {
+    imageUrl: '/images/firebase.png',
+    name: 'Firebase',
+    categorie: ['PlatForm'],
+    genle: ['BaaS'],
+  },
+  {
+    imageUrl: '/images/vercel.png',
+    name: 'Vercel',
+    categorie: ['PlatForm'],
+    genle: ['Hosting'],
+  },
+
+  {
+    imageUrl: '/images/latex.png',
+    name: 'LaTeX',
+    categorie: ['Software'],
+    genle: ['Word滅びろ'],
+  },
+]
 
 export const Skills = () => {
   return (
@@ -25,20 +129,16 @@ export const Skills = () => {
         animation={`${fadeIn} 1s forwards 3s`}
       >
         <SimpleGrid gap={'5px'} columns={[1, 2, 3]}>
-          <SkillCard
-            imageUrl="/images/javascript.png"
-            categorie={['language']}
-            genle={['web', 'front-end']}
-          >
-            JavaScript
-          </SkillCard>
-          <SkillCard
-            imageUrl="/images/typescript.png"
-            categorie={['language']}
-            genle={['web', 'front-end', 'back-end']}
-          >
-            TypeScript
-          </SkillCard>
+          {skills.map((skill) => (
+            <SkillCard
+              imageUrl={skill.imageUrl}
+              categorie={skill.categorie}
+              genle={skill.genle}
+              key={skill.name}
+            >
+              {skill.name}
+            </SkillCard>
+          ))}
         </SimpleGrid>
         <LinkButton href="skills">もっと見る</LinkButton>
       </Box>
