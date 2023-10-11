3925c9c0d3fe8c712ab29fd49d728f207520040f
okamonster <okamonster0615@gmail.com>
2023-10-12 00:59:43 +0900

    feat:add article

---
+++ src/components/Blog/Article/index.tsx
@@ -0,0 +1,18 @@
+import { Blog } from '@/entitie/blog'
+import { Box } from '@chakra-ui/react'
+import styles from './style.module.scss'
+type Props = {
+  blog: Blog
+}
+
+export const Article = ({ blog }: Props): React.ReactElement => {
+  console.log(blog.content)
+  return (
+    <Box p={['30px', '30px 85px']} bgColor={'#FFFFFF'} borderRadius={'lg'}>
+      <div
+        dangerouslySetInnerHTML={{ __html: blog.content }}
+        className={styles.post}
+      />
+    </Box>
+  )
+}
