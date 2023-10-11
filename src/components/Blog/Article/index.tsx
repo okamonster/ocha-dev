db46ef4b6c829c57709e1dd57735bf217dde5128
okamonster <okamonster0615@gmail.com>
2023-10-12 01:34:51 +0900

    fix:width

--- src/components/Blog/Article/index.tsx
+++ src/components/Blog/Article/index.tsx
@@ -8,7 +8,12 @@ type Props = {
 export const Article = ({ blog }: Props): React.ReactElement => {
   console.log(blog.content)
   return (
-    <Box p={['30px', '30px 85px']} bgColor={'#FFFFFF'} borderRadius={'lg'}>
+    <Box
+      p={['30px', '30px 85px']}
+      bgColor={'#FFFFFF'}
+      borderRadius={'lg'}
+      w={'100%'}
+    >
       <div
         dangerouslySetInnerHTML={{ __html: blog.content }}
         className={styles.post}
